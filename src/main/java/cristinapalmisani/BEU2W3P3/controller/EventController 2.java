93715e98a9710edd2cbb5f783c9da80d94895e9c
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-26 12:55:31 +0100

    correct commit

--- src/main/java/cristinapalmisani/BEU2W3P3/controller/EventController 2.java
+++
@@ -1,9 +0,0 @@
-package cristinapalmisani.BEU2W3P3.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/events")
-public class EventController {
-}
--- src/main/java/cristinapalmisani/BEU2W3P3/entities/Role 2.java
+++
@@ -1,6 +0,0 @@
-package cristinapalmisani.BEU2W3P3.entities;
-
-public enum Role {
-    USER,
-    EVENT_MANAGER
-}
