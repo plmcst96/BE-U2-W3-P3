0159d29c4c76191b8f513b40768fee6aa89f3713
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-26 10:35:08 +0100

    initial commit

---
+++ src/main/java/cristinapalmisani/BEU2W3P3/BeU2W3P3Application.java
@@ -0,0 +1,13 @@
+package cristinapalmisani.BEU2W3P3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2W3P3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2W3P3Application.class, args);
+	}
+
+}
---
+++ src/test/java/cristinapalmisani/BEU2W3P3/BeU2W3P3ApplicationTests.java
@@ -0,0 +1,13 @@
+package cristinapalmisani.BEU2W3P3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W3P3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
